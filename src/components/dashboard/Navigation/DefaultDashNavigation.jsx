c4510375b28e3e4e82476dd79083c171edaae42d
HIMU005 <hashanuzzaman99@gmail.com>
2025-02-28 16:22:56 +0600

    add common route.. now time for make axios secure

--- src/components/dashboard/Navigation/DefaultDashNavigation.jsx
+++ src/components/dashboard/Navigation/DefaultDashNavigation.jsx
@@ -1,13 +1,26 @@
+import { FaUserFriends } from "react-icons/fa";
 import { IoLogOutOutline } from "react-icons/io5";
+import useAuth from "../../../hooks/useAuth";
 import EachNavigationItem from "./EachNavigationItem";
 
 const DefaultDashNavigation = () => {
+  const { logOut } = useAuth();
   return (
     <div>
       <EachNavigationItem icon={IoLogOutOutline} title={"Logout"} />
       <EachNavigationItem icon={IoLogOutOutline} title={"Logout"} />
-      <EachNavigationItem icon={IoLogOutOutline} title={"Logout"} />
-      <EachNavigationItem icon={IoLogOutOutline} title={"Logout"} />
+      <EachNavigationItem
+        icon={FaUserFriends}
+        title={"Profile"}
+        direction={"profile"}
+      />
+
+      {/* logout at the end  */}
+      <EachNavigationItem
+        icon={IoLogOutOutline}
+        title={"Logout"}
+        action={logOut}
+      />
     </div>
   );
 };
