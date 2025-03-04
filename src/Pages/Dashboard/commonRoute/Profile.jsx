4b17a3a81b144b6b5a3183936b4bbfaf6922cdaa
HIMU005 <hashanuzzaman99@gmail.com>
2025-03-04 16:04:04 +0600

    user data and user based information are fetched from backend without any error

--- src/Pages/Dashboard/commonRoute/Profile.jsx
+++ src/Pages/Dashboard/commonRoute/Profile.jsx
@@ -1,4 +1,50 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
+import { axiosSecure } from "../../../hooks/useAxiosSecure";
+
 const Profile = () => {
+  const { user } = useAuth();
+  const { email } = user;
+
+  const [userInfo, setUserInfo] = useState(null);
+  const [roleInfo, setRoleInfo] = useState(null);
+  console.log(roleInfo);
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+
+  useEffect(() => {
+    if (userInfo) {
+      fetchRoleInfo();
+    }
+  }, [userInfo]);
+
+  const fetchUserInfo = async () => {
+    try {
+      const { data } = await axiosSecure(`/api/user/${email}`);
+      setUserInfo(data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchRoleInfo = async () => {
+    try {
+      // const {data} = await axiosSecure(`/api/`)
+      console.log(userInfo);
+      if (userInfo.role === "STUDENT") {
+        const { data } = await axiosSecure(
+          `/api/student/user_id/${userInfo.id}`
+        );
+        console.log(data);
+        setRoleInfo(data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>profile</div>;
 };
 
