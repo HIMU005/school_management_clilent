db7b4e6506b059b0517c8f50af3c6638f98f351c
HIMU005 <hashanuzzaman99@gmail.com>
2025-03-06 14:48:00 +0600

    fix the dashboard layout

--- src/layout/DashBoardLayout.jsx
+++ src/layout/DashBoardLayout.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
 import DashboardNav from "../components/dashboard/shared/DashboardNav";
 
 const DashBoardLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex  justify-start gap-2">
-      <DashboardNav />
-      <Outlet />
+      <DashboardNav isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div
+        className={`transition-all duration-500 flex-1 ${
+          isOpen ? "md:ml-96" : "md:ml-0"
+        }`}
+      >
+        <Outlet />
+      </div>
     </div>
   );
 };
