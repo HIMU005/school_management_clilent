745c41e9ed97d2eeb29a5c284fd5f9fac2b366bc
HIMU005 <hashanuzzaman99@gmail.com>
2025-03-11 16:33:45 +0600

    Now teacher can update their profile

--- src/components/dashboard/UpdateForm/TeacherUpdate.jsx
+++ src/components/dashboard/UpdateForm/TeacherUpdate.jsx
@@ -1,26 +1,55 @@
-import { Form } from "antd";
+import { Form, Input } from "antd";
 import { useEffect } from "react";
 
 /* eslint-disable react/prop-types */
 const TeacherUpdate = ({ roleInfo, form }) => {
-  console.log(roleInfo);
+  // console.log(roleInfo);
 
   useEffect(() => {
     if (roleInfo) {
-      form.setFieldValues({
+      form.setFieldsValue({
         roleData: {
-          id: roleInfo.id,
-          position: roleInfo.position || "",
-          passedFrom: roleInfo.passedFrom || "",
+          id: roleInfo?.id,
+          position: roleInfo?.position || "",
+          passedFrom: roleInfo?.passedFrom || "",
           joiningDate: roleInfo?.joiningDate,
         },
       });
     }
-  }, []);
+  }, [roleInfo, form]);
 
   return (
     <>
-      <Form.Item></Form.Item>
+      <Form.Item name={["roleData", "id"]} label={"Teacher Id :"}>
+        <Input disabled />
+      </Form.Item>
+
+      <Form.Item
+        name={["roleData", "position"]}
+        label={"Position :"}
+        rules={[
+          {
+            required: true,
+            message: "Enter your position in this organization",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name={["roleData", "passedFrom"]}
+        label={"Passed From :"}
+        rules={[
+          { required: true, message: "Enter your college / varsity name " },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item name={["roleData", "joiningDate"]} label={"Joining Date :"}>
+        <Input disabled />
+      </Form.Item>
     </>
   );
 };
